fb38c806d436dbeacd480e9ee739c0c3f2707e7e
Niely <niely.alberti@gmail.com>
2026-09-21 19:31:05 -0300

    Implementa listagem de produtos cadastrados

--- src/ProdutosDAO.java
+++ src/ProdutosDAO.java
@@ -33,7 +33,7 @@ public class ProdutosDAO {
             prep.setString(3, produto.getStatus());
 
             prep.executeUpdate();
-            
+
             return true;
 
         } catch (Exception erro) {
@@ -43,6 +43,36 @@ public class ProdutosDAO {
     }
 
     public ArrayList<ProdutosDTO> listarProdutos() {
+
+        listagem.clear();
+
+        String sql = "SELECT * FROM produtos";
+
+        try {
+            conn = new conectaDAO().connectDB();
+
+            prep = conn.prepareStatement(sql);
+
+            resultset = prep.executeQuery();
+
+            while (resultset.next()) {
+
+                ProdutosDTO produto = new ProdutosDTO();
+
+                produto.setId(resultset.getInt("id"));
+                produto.setNome(resultset.getString("nome"));
+                produto.setValor(resultset.getInt("valor"));
+                produto.setStatus(resultset.getString("status"));
+
+                listagem.add(produto);
+            }
+
+        } catch (Exception erro) {
+            JOptionPane.showMessageDialog(
+                    null,
+                    "Erro ao listar produtos: " + erro.getMessage()
+            );
+        }
 
         return listagem;
     }
